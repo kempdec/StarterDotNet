8f8f9ba409f0cf407f4a720fe1e53418cb254bb5
Vinícius Lima <viniciusxdl@outlook.com>
2023-12-10 12:10:22 -0300

    Corrigir AssemblyHelper.
    Corrigido um problema em que o assembly usado em AssemblyHelper era o do StarterDotNet, ao invés do assembly que está consumindo os métodos.

--- src/StarterDotNet.Reflection/AssemblyHelper.cs
+++ src/StarterDotNet.Reflection/AssemblyHelper.cs
@@ -8,35 +8,35 @@ namespace KempDec.StarterDotNet.Reflection;
 public static class AssemblyHelper
 {
     /// <summary>
-    /// Obtém todas as classes do assembly em execução, se houver alguma, que implemente o tipo da interface
-    /// especificado.
+    /// Obtém todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver alguma, que
+    /// implemente o tipo da interface especificado.
     /// </summary>
     /// <remarks>É necessário que as classes tenham um construtor público vazio.</remarks>
     /// <typeparam name="T">O tipo da interface das classes.</typeparam>
-    /// <returns>Todas as classes do assembly em execução, se houver alguma, que implemente o tipo da interface
-    /// especificado.</returns>
+    /// <returns>Todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver alguma, que
+    /// implemente o tipo da interface especificado.</returns>
     public static IEnumerable<T?> GetAllClassesWithInterface<T>() =>
-        Assembly.GetExecutingAssembly().GetAllClassesWithInterface<T>();
+        Assembly.GetCallingAssembly().GetAllClassesWithInterface<T>();
 
     /// <summary>
-    /// Obtém todas as classes do assembly em execução, se houver alguma, que implemente o tipo da interface
-    /// especificado.
+    /// Obtém todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver alguma, que
+    /// implemente o tipo da interface especificado.
     /// </summary>
     /// <remarks>É necessário que as classes tenham um construtor público vazio.</remarks>
     /// <typeparam name="T">O tipo das classes.</typeparam>
     /// <param name="interfaceType">O tipo da interface das classes.</param>
-    /// <returns>Todas as classes do assembly em execução, se houver alguma, que implemente o tipo da interface
-    /// especificado.</returns>
+    /// <returns>Todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver alguma, que
+    /// implemente o tipo da interface especificado.</returns>
     public static IEnumerable<T?> GetAllClassesWithInterface<T>(Type interfaceType) =>
-        Assembly.GetExecutingAssembly().GetAllClassesWithInterface<T>(interfaceType);
+        Assembly.GetCallingAssembly().GetAllClassesWithInterface<T>(interfaceType);
 
     /// <summary>
-    /// Obtém os tipos de todas as classes do assembly em execução, se houver alguma, que implemente o tipo da
-    /// interface especificado.
+    /// Obtém os tipos de todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver alguma,
+    /// que implemente o tipo da interface especificado.
     /// </summary>
     /// <param name="interfaceType">O tipo da interface das classes.</param>
-    /// <returns>Os tipos de todas as classes do assembly em execução, se houver alguma, que implemente o tipo da
-    /// interface especificado.</returns>
+    /// <returns>Os tipos de todas as classes do assembly de <see cref="Assembly.GetCallingAssembly"/>, se houver
+    /// alguma, que implemente o tipo da interface especificado.</returns>
     public static IEnumerable<Type> GetAllClassesWithInterface(Type interfaceType) =>
-        Assembly.GetExecutingAssembly().GetAllClassesWithInterface(interfaceType);
+        Assembly.GetCallingAssembly().GetAllClassesWithInterface(interfaceType);
 }
