619c4e19f1f21027ad3f25ca9d0ede506e10ff33
Vinícius Lima <viniciusxdl@outlook.com>
2023-12-10 08:32:03 -0300

    Refatorar.

--- samples/StarterDotNet.Sample/Program.cs
+++ samples/StarterDotNet.Sample/Program.cs
@@ -1,14 +1,12 @@
 using StarterDotNet.Sample.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
